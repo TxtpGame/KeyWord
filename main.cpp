e3d8869681f3e8bf82b8bb3f78617e5ab10648ff
TxtpGame <993823780@qq.com>
2022-07-12 19:58:57 +0800

    opt

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@ int main()
 {
 
     kw::init("words.txt");
-
     {
-        char str[1024] = u8"123𪜀𪜀爱𪜀𪜀321";
+        char str[1024] = u8"my shiter go";
         printf("===>:%s\r\n", str);
         if (kw::hexie(str) == 0)
         {
@@ -21,17 +20,30 @@ int main()
         }
     }
 
-    {
-        char str[1024] = u8"123愛る𪜀321";
-        printf("===>:%s\r\n", str);
-        char hint[1024] = "";
-        if (kw::check(str, hint) == 0)
-        {
-            printf("===>:ok\r\n");
-        }
-        else
-        {
-            printf("===>:err hint:%s\r\n", hint);
-        }
-    }
+    // {
+    //     char str[1024] = u8"123𪜀𪜀爱𪜀𪜀321";
+    //     printf("===>:%s\r\n", str);
+    //     if (kw::hexie(str) == 0)
+    //     {
+    //         printf("===>:ok\r\n");
+    //     }
+    //     else
+    //     {
+    //         printf("===>:err %s\r\n", str);
+    //     }
+    // }
+
+    // {
+    //     char str[1024] = u8"123愛る𪜀321";
+    //     printf("===>:%s\r\n", str);
+    //     char hint[1024] = "";
+    //     if (kw::check(str, hint) == 0)
+    //     {
+    //         printf("===>:ok\r\n");
+    //     }
+    //     else
+    //     {
+    //         printf("===>:err hint:%s\r\n", hint);
+    //     }
+    // }
 }
